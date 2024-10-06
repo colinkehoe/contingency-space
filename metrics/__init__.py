c887f5abcaeffede6e36c6b59cc0dbe2c0e60ff7
Colin Kehoe <kehoecolin012@gmail.com>
2024-10-06 15:19:20 -0500

    Quick import fix

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,2 +1,7 @@
 from metrics.acc import accuracy
+from metrics.pre import precision
+from metrics.rec import recall
+from metrics.tau import tau
 from metrics.imbalance_sensitivity import imbalance_sensitivity
+
+from metrics.average import Average
